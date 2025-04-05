d8101ba796a163c2e8c140f73e47da2686551ed4
Hala Belamri <100871287+hlbels@users.noreply.github.com>
2025-04-05 09:01:43 -0400

    Update main.cpp

--- Virtual Memory Manager/main.cpp
+++ Virtual Memory Manager/main.cpp
@@ -83,3 +83,90 @@ public:
         : id(id), start(start), BurstTime(BurstTime) {}
 
 };
+
+// ===== Hala's Contribution =====
+
+class VirtualMemoryManager {
+public:
+    int current_time = 0;
+    MainMemory MainMemory;
+    DiskMemory DiskMemory;
+
+    VirtualMemoryManager(int memSize) : MainMemory(memSize) {}
+
+    void Store(string variableId, unsigned int value) {
+        Page* page = new Page{variableId, value, current_time};
+
+        if (!MainMemory.IsFull()) {
+            MainMemory.readPage(page);
+        } else {
+            cout << "Main memory is full. Storing variable on disk." << endl;
+            DiskMemory.writePage(page); // To be implemented
+        }
+    }
+
+    void Load(string variableId) {
+        for (auto& page : MainMemory.getPages()) {
+            if (page->variableId == variableId) {
+                page->LastAccessValue = current_time;
+                cout << "Loaded from main memory: " << variableId
+                     << " = " << page->value << endl;
+                return;
+            }
+        }
+        cout << "Page not found in main memory. Trying to load from disk..." << endl;
+        DiskMemory.readPageFromFile(variableId);
+    }
+
+    void PrintMemoryState() {
+        cout << "---------- Main Memory ----------" << endl;
+        for (auto& page : MainMemory.getPages()) {
+            cout << "Variable: " << page->variableId
+                 << ", Value: " << page->value
+                 << ", Last Access Time: " << page->LastAccessValue << endl;
+        }
+
+        cout << "---------- Disk Memory ----------" << endl;
+        DiskMemory.printDiskContents(); // To be implemented
+    }
+
+    void AdvanceTime() {
+        current_time++;
+    }
+};
+
+// Extension to MainMemory: allow access to internal pages
+vector<Page*>& MainMemory::getPages() {
+    return pages;
+}
+
+// DiskMemory: function to simulate reading from disk
+void DiskMemory::readPageFromFile(const string& variableId) {
+    ifstream file("disk_storage.txt");
+    if (!file.is_open()) {
+        cout << "Disk file not found!" << endl;
+        return;
+    }
+
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        size_t pos = line.find(':');
+        if (pos != string::npos) {
+            string var = line.substr(0, pos);
+            string val = line.substr(pos + 1);
+            if (var == variableId) {
+                cout << "Loaded from disk: " << var << " = " << val << endl;
+                found = true;
+                break;
+            }
+        }
+    }
+
+    if (!found) {
+        cout << "Variable not found on disk." << endl;
+    }
+
+    file.close();
+}
